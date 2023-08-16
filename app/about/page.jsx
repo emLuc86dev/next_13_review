a563dd69b74e08bfd429a2ff97c5493f4a05c68b
EmilioAlphalabs <esmcode18@gmail.com>
2023-08-15 20:32:49 -0400

    first view

---
+++ app/about/page.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export const metadata = {
+  title: "Emilio Salvia Mendoza",
+};
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Emilio Salvia</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis eos
+        aperiam illo velit nulla minus nobis, asperiores possimus explicabo
+        maxime, laudantium quaerat facilis at aliquid distinctio necessitatibus
+        modi ab non!
+      </p>
+      <Link href="/">Back to Home</Link>
+    </div>
+  );
+};
+
+export default AboutPage;
